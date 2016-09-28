d359324cf5c2b6b660bb5e7167d05b177a2f3d05
woemike <ionmihai09@gmail.com>
2016-09-28 02:13:47 +0100

    whole project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Android: From Beginner to Paid Professional
+### Udemy course
+###### Hero Me is just a fun game where a user selects how he wants to become a superhero. The app is based on a strong and beautiful design.
+
+![alt tag](https://raw.github.com/woemike/HeroMe/master/Pictures/image.png)
---
+++ app/src/main/java/com/devslopes/herome/Activities/MainActivity.java
@@ -0,0 +1,48 @@
+package com.devslopes.herome.Activities;
+
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
+
+import com.devslopes.herome.Fragments.MainFragment;
+import com.devslopes.herome.Fragments.PickPowerFragment;
+import com.devslopes.herome.R;
+
+public class MainActivity extends AppCompatActivity implements MainFragment.MainFragmentInteractionListener, PickPowerFragment.PickPowerInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new MainFragment();
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.add(R.id.fragment_container, fragment);
+            transaction.commit();
+        }
+
+    }
+
+    public void loadPickPowerScreen() {
+
+        PickPowerFragment pickPowerFragment = new PickPowerFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, pickPowerFragment).addToBackStack(null).commit();
+
+    }
+
+    @Override
+    public void onMainFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onPickPowerFragmentInteraction(Uri uri) {
+
+    }
+}
